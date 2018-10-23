7c550699455a2c234d00a530b59e92ef3d9a1cbe
yandong.wang <yandong.wang@ishansong.com>
2018-10-23 19:34:17 +0800

    算法与数据结构

---
+++ algorithm/src/main/java/wang/xiaoluobo/StackTest.java
@@ -0,0 +1,39 @@
+package wang.xiaoluobo;
+
+import java.util.Stack;
+
+/**
+ * 两个栈实现队列
+ *
+ * @author wangyd
+ * @date 2018/10/23
+ */
+public class StackTest {
+
+    private static Stack<Integer> pushStack = new Stack<>();
+    private static Stack<Integer> popStack = new Stack<>();
+
+    private static void push(int node) {
+        pushStack.push(node);
+    }
+
+    private static int pop() {
+        if(popStack.isEmpty()){
+            while(!pushStack.isEmpty()){
+                popStack.push(pushStack.pop());
+            }
+        }
+        return popStack.pop();
+    }
+
+    public static void main(String[] args) {
+        push(1);
+        push(2);
+        System.out.println(pop());
+        push(3);
+        push(4);
+        System.out.println(pop());
+        System.out.println(pop());
+        System.out.println(pop());
+    }
+}
