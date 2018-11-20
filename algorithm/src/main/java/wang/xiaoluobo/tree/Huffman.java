db6a26710d7ca4be57232e7b0cf8ef4da398af97
yandong.wang <yandong.wang@ishansong.com>
2018-11-20 19:49:37 +0800

    zk fast paxos

---
+++ algorithm/src/main/java/wang/xiaoluobo/tree/Huffman.java
@@ -0,0 +1,12 @@
+package wang.xiaoluobo.tree;
+
+/**
+ * https://www.jianshu.com/p/a30953ab62a6
+ * https://blog.csdn.net/kimylrong/article/details/17022319
+ *
+ * @author wangyd
+ * @date 2018/11/19
+ */
+public class Huffman {
+
+}
