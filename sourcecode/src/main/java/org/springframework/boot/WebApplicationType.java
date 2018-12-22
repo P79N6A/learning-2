4cad8b6c62a1e66d7aa7c27de891078d1373d480
dongzai1005 <wangyd1005sy@163.com>
2018-12-23 07:49:35 +0800

    spring boot启动过程

---
+++ sourcecode/src/main/java/org/springframework/boot/WebApplicationType.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2012-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.boot;
+
+import org.springframework.util.ClassUtils;
+
+/**
+ * An enumeration of possible types of web application.
+ *
+ * @author Andy Wilkinson
+ * @author Brian Clozel
+ * @since 2.0.0
+ */
+public enum WebApplicationType {
+
+	/**
+	 * The application should not run as a web application and should not start an
+	 * embedded web server.
+	 *
+	 * 应用程序不应作为Web应用程序运行，也不应启动嵌入式Web服务器。
+	 */
+	NONE,
+
+	/**
+	 * The application should run as a servlet-based web application and should start an
+	 * embedded servlet web server.
+	 *
+	 * 应用程序应作为基于servlet的Web应用程序运行，并应启动嵌入式servlet Web服务器。
+	 */
+	SERVLET,
+
+	/**
+	 * The application should run as a reactive web application and should start an
+	 * embedded reactive web server.
+	 *
+	 * 应用程序应作为响应式Web应用程序运行，并应启动嵌入式响应式Web服务器。
+	 */
+	REACTIVE;
+
+	private static final String[] SERVLET_INDICATOR_CLASSES = { "javax.servlet.Servlet",
+			"org.springframework.web.context.ConfigurableWebApplicationContext" };
+
+	private static final String WEBMVC_INDICATOR_CLASS = "org.springframework."
+			+ "web.servlet.DispatcherServlet";
+
+	private static final String WEBFLUX_INDICATOR_CLASS = "org."
+			+ "springframework.web.reactive.DispatcherHandler";
+
+	private static final String JERSEY_INDICATOR_CLASS = "org.glassfish.jersey.servlet.ServletContainer";
+
+	private static final String SERVLET_APPLICATION_CONTEXT_CLASS = "org.springframework.web.context.WebApplicationContext";
+
+	private static final String REACTIVE_APPLICATION_CONTEXT_CLASS = "org.springframework.boot.web.reactive.context.ReactiveWebApplicationContext";
+
+	/**
+	 * 返回web应用类型
+	 * @return 默认返回{@code WebApplicationType.SERVLET}
+	 */
+	static WebApplicationType deduceFromClasspath() {
+		if (ClassUtils.isPresent(WEBFLUX_INDICATOR_CLASS, null)
+				&& !ClassUtils.isPresent(WEBMVC_INDICATOR_CLASS, null)
+				&& !ClassUtils.isPresent(JERSEY_INDICATOR_CLASS, null)) {
+			return WebApplicationType.REACTIVE;
+		}
+		for (String className : SERVLET_INDICATOR_CLASSES) {
+			if (!ClassUtils.isPresent(className, null)) {
+				return WebApplicationType.NONE;
+			}
+		}
+		return WebApplicationType.SERVLET;
+	}
+
+	static WebApplicationType deduceFromApplicationContext(
+			Class<?> applicationContextClass) {
+		if (isAssignable(SERVLET_APPLICATION_CONTEXT_CLASS, applicationContextClass)) {
+			return WebApplicationType.SERVLET;
+		}
+		if (isAssignable(REACTIVE_APPLICATION_CONTEXT_CLASS, applicationContextClass)) {
+			return WebApplicationType.REACTIVE;
+		}
+		return WebApplicationType.NONE;
+	}
+
+	private static boolean isAssignable(String target, Class<?> type) {
+		try {
+			return ClassUtils.resolveClassName(target, null).isAssignableFrom(type);
+		}
+		catch (Throwable ex) {
+			return false;
+		}
+	}
+
+}
