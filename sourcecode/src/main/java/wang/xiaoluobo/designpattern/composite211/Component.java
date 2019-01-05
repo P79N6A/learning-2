4bc089b233e4081b384327568c42769c166cc265
dongzai1005 <wangyd1005sy@163.com>
2019-01-05 11:00:26 +0800

    composite

---
+++ sourcecode/src/main/java/wang/xiaoluobo/designpattern/composite211/Component.java
@@ -0,0 +1,11 @@
+package wang.xiaoluobo.designpattern.composite211;
+
+public interface Component {
+
+    /**
+     * 输出组件自身的名称
+     *
+     * @param preStr 前缀
+     */
+    void printStruct(String preStr);
+}
