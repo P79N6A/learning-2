44f5632a017a8d54fea1dddd0a3e366b971e7963
dongzai1005 <wangyd1005sy@163.com>
2019-01-06 09:06:51 +0800

    memento

---
+++ sourcecode/src/main/java/wang/xiaoluobo/designpattern/memento319/Memento.java
@@ -0,0 +1,18 @@
+package wang.xiaoluobo.designpattern.memento319;
+
+public class Memento {
+
+    private String value;
+
+    public Memento(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
