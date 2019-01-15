18e75339ac19ee84f37f16b09c018d23412ebc17
yandong.wang <wangyd1005sy@163.com>
2019-01-15 19:30:04 +0800

    curator framework

---
+++ sourcecode/src/main/java/org/apache/curator/RetryPolicy.java
@@ -0,0 +1,40 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.curator;
+
+/**
+ * Abstracts the policy to use when retrying connections
+ *
+ * 重试策略顶级接口
+ */
+public interface RetryPolicy
+{
+    /**
+     * Called when an operation has failed for some reason. This method should return
+     * true to make another attempt.
+     *
+     * 由于某种原因操作失败时调用。 此方法应返回true以进行另一次尝试。
+     *
+     * @param retryCount the number of times retried so far (0 the first time)
+     * @param elapsedTimeMs the elapsed time in ms since the operation was attempted
+     * @param sleeper use this to sleep - DO NOT call Thread.sleep
+     * @return true/false
+     */
+    public boolean      allowRetry(int retryCount, long elapsedTimeMs, RetrySleeper sleeper);
+}
---
+++ sourcecode/src/main/java/org/apache/curator/curator.md
@@ -0,0 +1,4 @@
+# curator framework version 2.9.1
+> 核心类[CuratorFrameworkImpl](framework/imps/CuratorFrameworkImpl.java)，核心方法start()
+
+> 创建zk instance[HandleHolder](HandleHolder.java)，closeAndReset()方法
