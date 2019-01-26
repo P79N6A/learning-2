24d6f8235295817ec2b52792bbc00adf2f5f2bd8
dongzai1005 <wangyd1005sy@163.com>
2019-01-26 18:58:33 +0800

    leetcode

---
+++ algorithm/src/main/java/wang/xiaoluobo/leetcode/Problem0050.java
@@ -0,0 +1,28 @@
+package wang.xiaoluobo.leetcode;
+
+/**
+ * https://leetcode-cn.com/problems/powx-n/
+ */
+public class Problem0050 {
+
+    public static void main(String[] args) {
+        Problem0050 problem0050 = new Problem0050();
+        System.out.println(problem0050.myPow(2, 10));
+        System.out.println(problem0050.myPow(2, -2));
+    }
+
+    public double myPow(double x, int n) {
+        if (n == 1) {
+            return x;
+        }
+
+        if (n < 0) {
+            return 1 / myPow(x, -n);
+        }
+
+        if (x % 2 == 0) {
+            return x * myPow(x, n - 1);
+        }
+        return myPow(x * x, n / 2);
+    }
+}
