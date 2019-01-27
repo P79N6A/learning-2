734ea8b4c81ea2efe917f4e6f2f363ca12f464a5
dongzai1005 <wangyd1005sy@163.com>
2019-01-27 22:11:52 +0800

    leetcode

--- algorithm/src/main/java/wang/xiaoluobo/leetcode/Problem0053.java
+++ algorithm/src/main/java/wang/xiaoluobo/leetcode/Problem0053.java
@@ -10,6 +10,12 @@ public class Problem0053 {
         System.out.println(problem0053.maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
     }
 
+    /**
+     * 比较前N项中最大子数组的和与前N+1项中最大子数组的和相比较大小即可
+     *
+     * @param nums
+     * @return
+     */
     public int maxSubArray(int[] nums) {
         int sum = 0, max = nums[0];
         for (int i = 0; i < nums.length; i++) {
