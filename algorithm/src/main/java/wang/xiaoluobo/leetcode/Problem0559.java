57050b46acb7418d5d5a6d91e2cf9bfa2702f3e7
dongzai1005 <wangyd1005sy@163.com>
2019-01-28 20:56:21 +0800

    leetcode

---
+++ algorithm/src/main/java/wang/xiaoluobo/leetcode/Problem0559.java
@@ -0,0 +1,49 @@
+package wang.xiaoluobo.leetcode;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * https://leetcode-cn.com/problems/maximum-depth-of-n-ary-tree/
+ */
+public class Problem0559 {
+
+    public static void main(String[] args) {
+        Problem0559 problem0559 = new Problem0559();
+        Node c5 = new Node(5, null);
+        Node c6 = new Node(6, null);
+        Node c1 = new Node(3, Arrays.asList(c5, c6));
+        Node c2 = new Node(2, null);
+        Node c3 = new Node(4, null);
+        Node node = new Node(1, Arrays.asList(c1, c2, c3));
+        System.out.println(problem0559.maxDepth(node));
+    }
+
+    public int maxDepth(Node root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int max = 1;
+        if(root.children != null) {
+            for (Node node : root.children) {
+                max = Math.max(max, maxDepth(node) + 1);
+            }
+        }
+        return max;
+    }
+
+    static class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {
+
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    }
+}
